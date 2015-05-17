97a1f13b6542db984417fa8af14ae057e8261847
AlexYukikaze <puellamagi999@gmail.com>
2015-05-17 22:28:44 +0300

    lexer speed optimization

--- JSONx/lexer.py
+++ JSONx/lexer.py
@@ -50,21 +50,18 @@ class JSONxLexer(object):
         self.position = 0
         self.line = 1
         self.parser_regex = None
-        self.function_patterns = []
-        self.group_names = {}
+        self.groups = {}
 
     def register_regex_pattern(self, *patterns):
         regex_parts = []
         counter = 0
-        for pattern, tag in patterns:
+        for args in patterns:
+            pattern, tag, handler = (args[0], args[1], args[2]) if len(args) == 3 else (args[0], args[1], None)
             group_name = 'GROUP_{0}_{1}'.format(counter, tag)
-            self.group_names[group_name] = tag
+            self.groups[group_name] = tag, handler
             regex_parts.append('(?P<{0}>{1})'.format(group_name, pattern))
             counter += 1
         self.parser_regex = re.compile('|'.join(regex_parts), re.UNICODE)
-
-    def register_function_pattern(self, *patterns):
-        self.function_patterns += patterns
 
     def parse(self):
         while self.position < self.length:
@@ -72,67 +69,39 @@ class JSONxLexer(object):
             match = self.parser_regex.match(self.source, self.position)
             if match:
                 group_name = match.lastgroup
-                tag = self.group_names[group_name]
+                tag, handler = self.groups[group_name]
+                text = match.group(group_name)
+                if handler:
+                    text = handler(self, text)
                 if tag:
-                    text = match.group(group_name)
                     token = JSONxToken(tag, text, line, self.position)
                     yield token
-                self.position = match.end()
-                continue
-
-            match = None
-            for pair in self.function_patterns:
-                func, tag = pair
-                match = func.regex.match(self.source, self.position)
-                if match:
-                    token = func(match, self)
-                    if tag:
-                        yield token
-                    break
-
-            if not match:
+            else:
                 raise exception.LexerException('Illegal character {0} at ({1}, {2})'
-                                               .format(self.source[self.position]
-                                                       .replace('\n', '\\n')
-                                                       .replace('\r', '\\r'), line, self.position),
-                                               (line, self.position))
+                                               .format(self.source[self.position].encode('unicode-escape'),
+                                                       line, self.position), (line, self.position))
 
             self.position = match.end()
         yield JSONxToken(Type.EOF, 'EOF', self.line, self.position)
         return
 
 
-def regex_pattern(rule):
-    pattern = re.compile(rule, re.UNICODE)
-
-    def decorator(func):
-        def _(*args, **kwargs):
-            return func(*args, **kwargs)
-        _.regex = pattern
-        return _
-    return decorator
-
-
-@regex_pattern(r'\n+')
-def newline(match, lexer):
-    text = match.group()
+def newline(lexer, text):
     lexer.line += text.count('\n')
 
 
-@regex_pattern(r'/\*(.|\n)*?\*/')
-def multi_comment(match, lexer):
-    lexer.line += match.group().count('\n')
+def comment(lexer, text):
+    lexer.line += text.count('\n')
 
 
-@regex_pattern(r'("(?:[^"\\]|\\.)*")')
-def string(match, lexer):
-    text = match.group()
+def string(lexer, text):
     text = utils.decode_escapes(text)
-    return JSONxToken(Type.STRING, text[1: -1], lexer.line, lexer.position)
+    return text[1: -1]
 
 
 regex_patterns = [
-    (r'[ \t\r]+', Type.IGNORE),
+    (r'(\r\n|\n|\r)+', Type.IGNORE, newline),
+    (r'[ \t]+', Type.IGNORE),
     (r'//[^\n]*', Type.IGNORE),
     (r'{', Type.LEFT_CURLY_BRACKET),
     (r'}', Type.RIGHT_CURLY_BRACKET),
@@ -144,17 +113,12 @@ regex_patterns = [
     (r'([+-]?(0|[1-9][0-9]*)(\.[0-9]*)?([eE][+-]?[0-9]+)?)', Type.NUMBER),
     (r'\b(true|false|null)\b', Type.KEYWORD),
     (r'[A-Za-z_][A-Za-z0-9_]*', Type.ID),
-]
-
-function_patterns = [
-    (newline, Type.IGNORE),
-    (multi_comment, Type.IGNORE),
-    (string, Type.STRING)
+    (r'("(?:[^"\\]|\\.)*")', Type.STRING, string),
+    (r'/\*(.|\n)*?\*/', Type.IGNORE, comment)
 ]
 
 
 def tokenize(source):
     lexer = JSONxLexer(source)
     lexer.register_regex_pattern(*regex_patterns)
-    lexer.register_function_pattern(*function_patterns)
     return [token for token in lexer.parse()]
