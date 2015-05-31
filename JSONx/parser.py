0b115a6b1a627e897892c5c6b10cdc072c414f5d
AlexYukikaze <puellamagi999@gmail.com>
2015-05-31 10:26:13 +0300

    parser speedup

--- JSONx/parser.py
+++ JSONx/parser.py
@@ -1,171 +1,157 @@
 __author__ = 'Alex'
 
-import lexer
-import ast
+from lexer import *
+from ast import *
 import exception
-from functools import partial
 
 
 class Parser(object):
     def __init__(self, tokens):
-        self.position = 0
         self.tokens = tokens
+        self.position = 0
+        self.length = len(tokens)
 
     def error(self, message, *args):
-        token = self.peek()
-        raise exception.ParserException(message.format(args, current=token), (token.line, token.position))
+        line, col = self.token.get_line_col()
+        raise exception.ParserException(message.format(*args), (line, col))
 
-    def peek(self):
-        return self.tokens[self.position]
+    @property
+    def token(self):
+        """
+        :rtype : Token
+        """
+        return self.tokens[self.position] if self.position < self.length else None
 
-    def skip(self, count=1):
-        self.position += count
-
-    def parse_type(self, token_type):
-        return partial(self.consume, token_type)
-
-    def check_type(self, token_type):
-        token = self.peek()
-        if token.type != token_type:
-            return False
-        self.skip()
-        return True
-
-    def ensure_type(self, token_type, message, *args):
-        token = self.peek()
-        if token.type != token_type:
-            self.error(message, args)
-        self.skip()
-        return token
-
-    def ensure(self, func, message, *args):
-        result = func()
-        if not result:
-            self.error(message, args)
-        return result
-
-    def attempt(self, func):
-        backup_pos = self.position
-        result = func()
-        if not result:
-            self.position = backup_pos
-        return result
-
-    def consume(self, token_type):
-        token = self.peek()
-        if token.type != token_type:
+    def expect(self, expected_type):
+        """
+        :rtype : Token | None
+        """
+        token = self.tokens[self.position] if self.position < self.length else None
+        if not token or token.type != expected_type:
             return None
-        self.skip()
+        self.position += 1
         return token
 
-    def sequence(self, *parser_funcs):
-        result = []
-        for parser in parser_funcs:
-            value = self.attempt(parser)
-            if not value:
-                return None
-            result.append(value)
-        return result
-
-    def repeat(self, parser_func, separator_parser):
-        result = []
-        val = parser_func()
-        while val:
-            result.append(val)
-            val = self.sequence(separator_parser, parser_func)
-            if val:
-                val = val[1]
-        return result
+    def ensure(self, expected_type, message, *args):
+        """
+        :rtype : Token | None
+        """
+        token = self.tokens[self.position] if self.position < self.length else None
+        if not token or token.type != expected_type:
+            self.error(message, *args)
+        self.position += 1
+        return token
 
 
 class JSONxParser(Parser):
     def parse_keyword(self):
-        token = self.consume(lexer.Type.KEYWORD)
+        token = self.expect(Type.KEYWORD)
         if not token:
             return None
         if token.value == 'true':
-            return ast.TrueNode(token)
+            return TrueNode(token)
         if token.value == 'false':
-            return ast.FalseNode(token)
+            return FalseNode(token)
         if token.value == 'null':
-            return ast.NullNode(token)
+            return NullNode(token)
 
     def parse_number(self):
-        token = self.consume(lexer.Type.NUMBER)
-        if not token:
-            return None
-        return ast.NumberNode(token)
+        token = self.expect(Type.NUMBER)
+        if token:
+            return NumberNode(token)
 
     def parse_string(self):
-        token = self.consume(lexer.Type.STRING)
-        if not token:
-            return None
-        return ast.StringNode(token)
-
-    def parse_identifier(self):
-        token = self.consume(lexer.Type.ID)
-        if not token:
-            return None
-        return ast.StringNode(token)
-
-    def parse_array(self):
-        if not self.check_type(lexer.Type.LEFT_SQUARE_BRACKET):
-            return None
-        if self.check_type(lexer.Type.RIGHT_SQUARE_BRACKET):
-            return ast.ArrayNode([])
-        value = self.repeat(self.parse_value, self.parse_type(lexer.Type.COMMA))
-        if not value:
-            self.error("Bad array: <value> expected, got '{current.value}'")
-        self.ensure_type(lexer.Type.RIGHT_SQUARE_BRACKET, "Bad array: <]> expected, got '{current.value}'")
-        return ast.ArrayNode(value)
+        token = self.expect(Type.STRING)
+        if token:
+            return StringNode(token)
 
     def parse_object(self):
-        if not self.check_type(lexer.Type.LEFT_CURLY_BRACKET):
+        left_bracket = self.expect(Type.LEFT_CURLY_BRACKET)
+        if not left_bracket:
             return None
-        if self.check_type(lexer.Type.RIGHT_CURLY_BRACKET):
-            return ast.ObjectNode([])
-        pairs = self.repeat(self.parse_pair, self.parse_type(lexer.Type.COMMA))
-        self.ensure_type(lexer.Type.RIGHT_CURLY_BRACKET, "Bad object: <}}> expected, got '{current.value}'")
-        return ast.ObjectNode(pairs)
+        right_bracket = self.expect(Type.RIGHT_CURLY_BRACKET)
+        if right_bracket:
+            return ObjectNode([])
+        pairs = self.parse_pairs()
+        self.ensure(Type.RIGHT_CURLY_BRACKET, 'expected "}}", got "{}"', self.token)
+        return ObjectNode(pairs)
+
+    def parse_pairs(self):
+        pairs = []
+        while True:
+            if pairs and not self.expect(Type.COMMA):
+                break
+            pair = self.parse_pair()
+            if not pair:
+                self.error('expected <pair>, got "{}"', self.token)
+            pairs += pair,
+        return pairs
 
     def parse_pair(self):
-        key = self.attempt(self.parse_string) or \
-              self.ensure(self.parse_identifier, "Bad pair: <key> expected, got '{current.value}'")
-        if not self.check_type(lexer.Type.COLON):
+        key = self.parse_string()
+        if not key:
             return None
-        value = self.ensure(self.parse_value, "Bad pair: <value> expected, got '{current.value}'")
-        return ast.PairNode(key, value)
+        self.ensure(Type.COLON, 'expected ":", got "{}"', self.token)
+        value = self.parse_value()
+        if not value:
+            self.error('expected <value>, got "{}"', self.token)
+        return PairNode(key, value)
+
+    def parse_array(self):
+        left_bracket = self.expect(Type.LEFT_SQUARE_BRACKET)
+        if not left_bracket:
+            return None
+        right_bracket = self.expect(Type.RIGHT_SQUARE_BRACKET)
+        if right_bracket:
+            return ArrayNode([])
+        elements = self.parse_elements()
+        self.ensure(Type.RIGHT_SQUARE_BRACKET, 'expected "]", got "{}"', self.token)
+        return ArrayNode(elements)
+
+    def parse_elements(self):
+        elements = []
+        while True:
+            if elements and not self.expect(Type.COMMA):
+                break
+            value = self.parse_value()
+            if not value:
+                self.error('expected <value>, got "{}"', self.token)
+            elements += value,
+        return elements
 
     def parse_reference(self):
-        def parse_body():
-            path = self.attempt(self.parse_string)
-            return ast.PairNode(ast.NullNode(None), path)
-
-        if not self.check_type(lexer.Type.DOLLAR):
+        if not self.expect(Type.DOLLAR):
             return None
-        self.ensure_type(lexer.Type.LEFT_CURLY_BRACKET, "Bad reference: <{{> expected, got '{current.value}'")
-        pair = self.attempt(self.parse_pair) or \
-               self.ensure(parse_body, "Bad reference: <reference_body> expected, got '{current.value}'")
-        self.ensure_type(lexer.Type.RIGHT_CURLY_BRACKET, "Bad reference: <}}> expected, got '{current.value}'")
-        return ast.ReferenceNode(pair)
+        self.ensure(Type.LEFT_CURLY_BRACKET, 'Bad reference: <{{> expected, got {}', self.token)
+        object_path = NullNode(None)
+        file_path = self.parse_string()
+        if self.expect(Type.COLON):
+            object_path = file_path
+            file_path = self.parse_string()
+        if not file_path:
+            self.error('Bad reference: <string> expected, got {}', self.token)
+        self.ensure(Type.RIGHT_CURLY_BRACKET, 'Bad reference: <}}> expected, got', self.token)
+        return ReferenceNode(PairNode(object_path, file_path))
 
     def parse_value(self):
-        tok_type = self.peek().type
-        if tok_type == lexer.Type.LEFT_SQUARE_BRACKET:
-            return self.parse_array()
-        elif tok_type == lexer.Type.LEFT_CURLY_BRACKET:
+        token = self.token
+        if token.type == Type.LEFT_CURLY_BRACKET:
             return self.parse_object()
-        elif tok_type == lexer.Type.KEYWORD:
-            return self.parse_keyword()
-        elif tok_type == lexer.Type.NUMBER:
-            return self.parse_number()
-        elif tok_type == lexer.Type.DOLLAR:
-            return self.parse_reference()
-        elif tok_type == lexer.Type.STRING:
+        if token.type == Type.LEFT_SQUARE_BRACKET:
+            return self.parse_array()
+        if token.type == Type.STRING:
             return self.parse_string()
+        if token.type == Type.NUMBER:
+            return self.parse_number()
+        if token.type == Type.KEYWORD:
+            return self.parse_keyword()
+        if token.type == Type.DOLLAR:
+            return self.parse_reference()
 
     def parse(self):
-        return self.ensure(self.parse_value, "Bad source: <value> expected, got '{current.value}'")
+        value = self.parse_value()
+        self.ensure(Type.EOF, 'Bad file: <EOF> expected, got "{}"', self.token)
+        return value
 
 
 def parse(tokens):
--- Tests/test_visitor.py
+++ Tests/test_visitor.py
@@ -16,54 +16,54 @@ class TestVisitor(unittest.TestCase):
         self.assertEqual(result, expected)
 
     def test_visit_number(self):
-        self.assert_visitor(NumberNode(JSONxToken(Type.NUMBER, '0', 0, 0)), 0)
-        self.assert_visitor(NumberNode(JSONxToken(Type.NUMBER, '123', 0, 0)), 123)
+        self.assert_visitor(NumberNode(JSONxToken(Type.NUMBER, '0', 0)), 0)
+        self.assert_visitor(NumberNode(JSONxToken(Type.NUMBER, '123', 0)), 123)
 
     def test_visit_string(self):
-        self.assert_visitor(StringNode(JSONxToken(Type.STRING, '', 0, 0)), '')
-        self.assert_visitor(StringNode(JSONxToken(Type.STRING, 'string', 0, 0)), 'string')
+        self.assert_visitor(StringNode(JSONxToken(Type.STRING, '', 0)), '')
+        self.assert_visitor(StringNode(JSONxToken(Type.STRING, 'string', 0)), 'string')
 
     def test_visit_true(self):
-        self.assert_visitor(TrueNode(JSONxToken(Type.KEYWORD, 'true', 0, 0)), True)
+        self.assert_visitor(TrueNode(JSONxToken(Type.KEYWORD, 'true', 0)), True)
 
     def test_visit_false(self):
-        self.assert_visitor(FalseNode(JSONxToken(Type.KEYWORD, 'false', 0, 0)), False)
+        self.assert_visitor(FalseNode(JSONxToken(Type.KEYWORD, 'false', 0)), False)
 
     def test_visit_null(self):
-        self.assert_visitor(NullNode(JSONxToken(Type.KEYWORD, 'null', 0, 0)), None)
+        self.assert_visitor(NullNode(JSONxToken(Type.KEYWORD, 'null', 0)), None)
 
     def test_visit_array(self):
         array_node = ArrayNode([
-            TrueNode(JSONxToken(Type.KEYWORD, 'true', 0, 0)),
-            FalseNode(JSONxToken(Type.KEYWORD, 'false', 0, 0)),
-            NullNode(JSONxToken(Type.KEYWORD, 'null', 0, 0))
+            TrueNode(JSONxToken(Type.KEYWORD, 'true', 0)),
+            FalseNode(JSONxToken(Type.KEYWORD, 'false', 0)),
+            NullNode(JSONxToken(Type.KEYWORD, 'null', 0))
         ])
         self.assert_visitor(array_node, [True, False, None])
 
     def test_visit_pair(self):
-        pair_node = PairNode(StringNode(JSONxToken(Type.STRING, 'key', 0, 0)),
-                             NumberNode(JSONxToken(Type.NUMBER, '123', 0, 0)))
+        pair_node = PairNode(StringNode(JSONxToken(Type.STRING, 'key', 0)),
+                             NumberNode(JSONxToken(Type.NUMBER, '123', 0)))
         self.assert_visitor(pair_node, ('key', 123))
 
     def test_visit_object(self):
-        pair_node = PairNode(StringNode(JSONxToken(Type.STRING, 'key', 0, 0)),
-                             NumberNode(JSONxToken(Type.NUMBER, '123', 0, 0)))
+        pair_node = PairNode(StringNode(JSONxToken(Type.STRING, 'key', 0)),
+                             NumberNode(JSONxToken(Type.NUMBER, '123', 0)))
         object_node = ObjectNode([pair_node])
         self.assert_visitor(object_node, {"key": 123})
 
     def test_visit_reference(self):
         ref_node = ReferenceNode(PairNode(
-            StringNode(JSONxToken(Type.STRING, 'file.xc', 0, 0)),
-            StringNode(JSONxToken(Type.STRING, '.path', 0, 0))
+            StringNode(JSONxToken(Type.STRING, 'file.xc', 0)),
+            StringNode(JSONxToken(Type.STRING, '.path', 0))
         ))
         self.assert_visitor(ref_node, {"$ref": {"file": "file.xc", "path": ".path"}})
 
     def test_visit_tree(self):
         array_node = ArrayNode([
-            TrueNode(JSONxToken(Type.KEYWORD, 'true', 0, 0)),
-            FalseNode(JSONxToken(Type.KEYWORD, 'false', 0, 0)),
-            NullNode(JSONxToken(Type.KEYWORD, 'null', 0, 0))
+            TrueNode(JSONxToken(Type.KEYWORD, 'true', 0)),
+            FalseNode(JSONxToken(Type.KEYWORD, 'false', 0)),
+            NullNode(JSONxToken(Type.KEYWORD, 'null', 0))
         ])
-        pair_node = PairNode(StringNode(JSONxToken(Type.STRING, 'array', 0, 0)), array_node)
+        pair_node = PairNode(StringNode(JSONxToken(Type.STRING, 'array', 0)), array_node)
         object_node = ObjectNode([pair_node])
         self.assert_visitor(object_node, {"array": [True, False, None]})
